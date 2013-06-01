4c13df77fcbd9b5f16fec87563be18527075bd9a
shishir <shishir.das@gmail.com>
2013-06-01 16:36:49 +0530

    more hadoop stuff

--- cookbooks/hadoop/attributes/default.rb
+++ cookbooks/hadoop/attributes/default.rb
@@ -5,4 +5,4 @@ node.default[:hadoop][:download_dir]    = "/tmp"
 node.default[:hadoop][:user]            = "hduser"
 node.default[:hadoop][:user_home]       = "/home/#{node.hadoop.user}"
 node.default[:hadoop][:user_group_name] = "hadoop"
-
+node.default[:hadoop][:data_dir]        = "#{node.hadoop.user_home}/app/hadoop/tmp"
--- cookbooks/hadoop/recipes/default.rb
+++ cookbooks/hadoop/recipes/default.rb
@@ -15,9 +15,17 @@ user node.hadoop.user do
 end
 
 execute "Create Ssh key for #{node.hadoop.user}" do
-  command "mkdir .ssh && ssh-keygen -f .ssh/id_rsa -P """
+  command "mkdir #{node.hadoop.user_home}/.ssh && ssh-keygen -f #{node.hadoop.user_home}/.ssh/id_rsa -P ''"
+  cwd node.hadoop.user_home
   user node.hadoop.user
-  not_if {::File.exists?(".ssh/id_rsa")}
+  not_if {::File.exists?("#{node.hadoop.user_home}/.ssh/id_rsa")}
+end
+
+execute "allow ssh access for local machine" do
+  command "cat #{node.hadoop.user_home}/.ssh/id_rsa.pub >> #{node.hadoop.user_home}/.ssh/authorized_keys"
+  cwd node.hadoop.user_home
+  user node.hadoop.user
+  not_if {::File.exists?("#{node.hadoop.user_home}/.ssh/authorized_keys")}
 end
 
 execute "unpack hadoop" do
@@ -28,3 +36,37 @@ end
 execute "transfer ownership to hduser" do
   command "chown -R #{node.hadoop.user}:#{node.hadoop.user_group_name} #{node.hadoop.user_home}"
 end
+
+execute "append java home to hadoop setenv.sh" do
+  command %Q{echo "export JAVA_HOME=$JAVA_HOME" >> "#{node.hadoop.user_home}/hadoop-1.2.0/conf/hadoop-env.sh"}
+  user node.hadoop.user
+end
+
+execute "create directory through execute, coz chef is stupid with permission on recursive" do
+  command "mkdir -p #{node.hadoop.data_dir}"
+  user node.hadoop.user
+  group node.hadoop.user_group_name
+  not_if {::File.exists?("#{node.hadoop.data_dir}")}
+end
+
+template "#{node.hadoop.user_home}/hadoop-1.2.0/conf/core-site.xml" do
+  owner node.hadoop.user
+  group node.hadoop.user_group_name
+  mode 0664
+  variables :data_dir => node.hadoop.data_dir
+end
+
+template "#{node.hadoop.user_home}/hadoop-1.2.0/conf/mapred-site.xml" do
+  owner node.hadoop.user
+  group node.hadoop.user_group_name
+  mode 0664
+end
+
+template "#{node.hadoop.user_home}/hadoop-1.2.0/conf/hdfs-site.xml" do
+  owner node.hadoop.user
+  group node.hadoop.user_group_name
+  mode 0664
+end
+
+
+
